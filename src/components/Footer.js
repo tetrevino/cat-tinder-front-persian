3130eedcb7840b6721098ee63d14359f002e2370
Michael Egner <148016951+MEgner410@users.noreply.github.com>
2024-02-06 14:59:42 -0800

    frontend structure completed

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FooterImage from './FooterImage.png'
+
+function Footer() {
+    return (
+      <footer>
+        <img src={FooterImage} alt="Footer" />
+      </footer>
+    );
+  }
+  
+  export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import HeaderImage from './HeaderImage.png'
+
+function Header() {
+    return (
+      <header>
+        <img src={HeaderImage} alt="Header" />
+      </header>
+    );
+  }
+  
+  export default Header;
